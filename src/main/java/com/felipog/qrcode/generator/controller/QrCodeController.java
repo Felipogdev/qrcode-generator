8a006c136c9fdf441d574840217c625a5ae86e8d
Felipe Laurentino <felipedantaslaurentinodev@gmail.com>
2025-08-06 19:13:25 +0100

    Feat: qrcodegenerator service

--- src/main/java/com/felipog/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/felipog/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.felipog.qrcode.generator.controller;
 
 import com.felipog.qrcode.generator.dto.QrCodeGenerateRequestDTO;
 import com.felipog.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.felipog.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qr-code")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
     @PostMapping
-    public ResponseEntity<QrCodeGenerateResponse
-            > generate(@RequestBody QrCodeGenerateRequestDTO request) {
-        return null;
+    public ResponseEntity<QrCodeGenerateResponse> generate(
+            @RequestBody QrCodeGenerateRequestDTO request
+    ) {
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
